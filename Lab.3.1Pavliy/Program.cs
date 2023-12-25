7e726a0a60cd0aa88870c6e56cf645d38d9a5847
Nikita Pavliy <nikitapavliy@gmail.com>
2023-12-25 23:47:29 +0200

    Добавьте файлы проекта.

---
+++ Lab.3.1Pavliy/Program.cs
@@ -0,0 +1,22 @@
+﻿int N = 100; 
+int maxDigit = 0; 
+
+for (int i = 10; i <= N; i++)
+{
+    int number = i;
+
+    while (number > 0)
+    {
+        int digit = number % 10; 
+
+        if (digit > maxDigit)
+        {
+            maxDigit = digit; 
+        }
+
+        number /= 10; 
+
+    }
+}
+
+Console.WriteLine($"Наибольшая цифра в диапазоне от 10 до {N}: {maxDigit}");
